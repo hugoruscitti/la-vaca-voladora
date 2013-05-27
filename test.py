ef36d2655e2146d3fd91a22cfb90f438a9e8ef06
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-05-27 12:49:47 -0300

    Agregando estados a la Vaca.

--- test.py
+++ test.py
@@ -8,27 +8,94 @@
 import random
 import pilas
 
-class Vaca(pilas.actores.Actor):
 
-    def __init__(self):
-        pilas.actores.Actor.__init__(self)
-        self.imagen = 'data/vaca_volando.png'
-        self.centro = (140, 59)
-        self.radio_de_colision = 40
-        self.x = -170
+class Estado:
+
+    def __init__(self, vaca):
+        self.vaca = vaca
+        self.iniciar()
+
+    def iniciar(self):
+        pass
+
+
+
+class Ingresando(Estado):
+
+    def iniciar(self):
+        self.contador = 0
+        self.vaca.x = -380
+        self.vaca.x = [-170], 0.5
+
+    def actualizar(self):
+        self.contador += 1
+
+        if self.contador > 50:
+            self.vaca.estado = Volando(self.vaca)
+
+class Volando(Estado):
+
+    def iniciar(self):
+        self.contador = 0
 
     def actualizar(self):
         velocidad = 5
 
         if pilas.mundo.control.arriba:
-            self.y += velocidad
+            self.vaca.y += velocidad
         elif pilas.mundo.control.abajo:
-            self.y -= velocidad
+            self.vaca.y -= velocidad
 
-        if self.y > 210:
-            self.y = 210
-        elif self.y < -210:
-            self.y = -210
+        if self.vaca.y > 210:
+            self.vaca.y = 210
+        elif self.vaca.y < -210:
+            self.vaca.y = -210
+
+        self.contador += 0.2
+
+        if (self.contador % 2) < 1:
+            self.vaca.imagen.definir_cuadro(3)
+        else:
+            self.vaca.imagen.definir_cuadro(4)
+
+
+class Perdiendo(Estado):
+
+    def iniciar(self):
+        self.vaca.imagen.definir_cuadro(0)
+        self.vaca.centro = ('centro', 'centro')
+        pass
+
+    def actualizar(self):
+        self.vaca.rotacion += 7
+        self.vaca.escala += 0.01
+        self.vaca.x += 1
+        self.vaca.y -= 1
+        pass
+
+
+
+
+class Vaca(pilas.actores.Actor):
+
+    def __init__(self):
+        pilas.actores.Actor.__init__(self)
+        grilla = pilas.imagenes.cargar_grilla('data/sprites.png', 5, 1)
+        grilla.definir_cuadro(3)
+        self.imagen = grilla
+        self.centro = (140, 59)
+        self.radio_de_colision = 40
+        self.x = -170
+        self.estado = Ingresando(self)
+
+    def actualizar(self):
+        self.estado.actualizar()
+
+    def perder(self):
+        self.estado = Perdiendo(self)
+        t = pilas.actores.Texto("Has perdido ...")
+        t.escala = 0
+        t.escala = [1], 0.5
 
 class Enemigo(pilas.actores.Bomba):
 
@@ -39,6 +106,7 @@ class Enemigo(pilas.actores.Bomba):
 
     def actualizar(self):
         self.x -= 5
+        pilas.actores.Bomba.actualizar(self)
 
 class Item(pilas.actores.Actor):
 
@@ -122,6 +190,7 @@ pilas.mundo.agregar_tarea(3.3, crear_enemigo)
 
 
 def cuanto_toca_enemigo(vaca, enemigo):
+    vaca.perder()
     enemigo.eliminar()
 
 
