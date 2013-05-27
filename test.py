d2b193395e24ded8d94c18afeb34902f56dc3dba
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-05-27 11:20:09 -0300

    Commit inicial

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Instrucciones:
+#
+#     para iniciar este juego, escribir:
+#
+#           python test.py
+
+import pilas
+
+pilas.iniciar()
+
+pelota = pilas.actores.Pelota()
+
+pilas.ejecutar()
