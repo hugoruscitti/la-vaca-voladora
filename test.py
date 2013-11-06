9699b85368afa0b4f65a4143aa820140655d222b
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-11-06 15:36:03 -0300

    Agregando una imagen para representar al jugador y otros cambios menores.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@
 #           python test.py
 
 import random
+import math
 import pilas
 
 
@@ -19,6 +20,42 @@ class Estado:
         pass
 
 
+class Jugador(pilas.actores.Actor):
+
+    def __init__(self):
+        self.carga_imagenes()
+        pilas.actores.Actor.__init__(self, self.normal)
+        self.contador_reposo = 0
+        self.izquierda =  -310
+        self.arriba = 225
+        pilas.eventos.pulsa_tecla.conectar(self.cuando_pulsa_tecla)
+
+    def carga_imagenes(self):
+        self.normal = pilas.imagenes.cargar("cara_normal.png")
+        self.gana = pilas.imagenes.cargar("cara_gana.png")
+        self.pierde = pilas.imagenes.cargar("cara_pierde.png")
+
+    def actualizar(self):
+        if self.contador_reposo > 0:
+            self.contador_reposo -= 1
+
+            if self.contador_reposo == 0:
+                self.imagen = self.normal
+
+    def mostrar_cara_gana(self):
+        self.imagen = self.gana
+        self.contador_reposo = 40
+
+    def mostrar_cara_pierde(self):
+        self.imagen = self.pierde
+        self.contador_reposo = 40
+
+    def cuando_pulsa_tecla(self, evento):
+        if evento.codigo == 'r':
+            self.carga_imagenes()
+            self.imagen = self.normal
+            print "recargando imagenes..."
+
 
 class Ingresando(Estado):
 
@@ -29,7 +66,7 @@ class Ingresando(Estado):
         self.vaca.x = [-170], 0.5
 
     def actualizar(self):
-        self.contador += 1
+        self.contador += 2
 
         if self.contador > 50:
             self.vaca.estado = Volando(self.vaca)
@@ -38,19 +75,46 @@ class Volando(Estado):
 
     def iniciar(self):
         self.vaca.definir_animacion([3, 4])
+        self.contador = 0
+        self.esta_riendo = False
 
     def actualizar(self):
+        control = pilas.mundo.control
         velocidad = 5
 
-        if pilas.mundo.control.arriba:
+        if control.arriba:
             self.vaca.y += velocidad
-        elif pilas.mundo.control.abajo:
+        elif control.abajo:
             self.vaca.y -= velocidad
+
+        if control.izquierda:
+            self.vaca.x -= velocidad
+        elif control.derecha:
+            self.vaca.x += velocidad
 
         if self.vaca.y > 210:
             self.vaca.y = 210
         elif self.vaca.y < -210:
             self.vaca.y = -210
+
+        if self.vaca.izquierda < -400:
+            self.vaca.izquierda = -400
+
+        if not self.esta_riendo:
+            if self.vaca.contador_sonreir == 40:
+                self.esta_riendo = True
+                self.vaca.definir_animacion([1, 2])
+        else:
+            if self.vaca.contador_sonreir == 0:
+                self.esta_riendo = False
+                self.vaca.definir_animacion([3, 4])
+
+        self.actualizar_movimiento_vuelo()
+
+    def actualizar_movimiento_vuelo(self):
+        "Mueve la vaca de arriba hacia abajo muy suavemente."
+        self.contador += 0.1
+        self.vaca.y += math.cos(self.contador) * 0.4
 
 
 class Perdiendo(Estado):
@@ -58,12 +122,14 @@ class Perdiendo(Estado):
     def iniciar(self):
         self.vaca.definir_animacion([0])
         self.vaca.centro = ('centro', 'centro')
+        self.contador = 0
 
     def actualizar(self):
-        self.vaca.rotacion += 7
-        self.vaca.escala += 0.01
-        self.vaca.x += 1
-        self.vaca.y -= 1
+        self.contador += 1
+        self.vaca.x -= 3
+
+        if self.contador > 30:
+            self.vaca.regresar_al_vuelo()
 
 
 class Vaca(pilas.actores.Actor):
@@ -78,6 +144,7 @@ class Vaca(pilas.actores.Actor):
         self.x = -170
         self.estado = Ingresando(self)
         self.contador = 0
+        self.contador_sonreir = 0
 
     def definir_animacion(self, cuadros):
         self.paso = 0
@@ -87,6 +154,9 @@ class Vaca(pilas.actores.Actor):
     def actualizar(self):
         self.estado.actualizar()
         self.actualizar_animacion()
+
+        if self.contador_sonreir > 0:
+            self.contador_sonreir -= 1
 
     def actualizar_animacion(self):
         self.contador += 0.2
@@ -102,9 +172,22 @@ class Vaca(pilas.actores.Actor):
 
     def perder(self):
         self.estado = Perdiendo(self)
-        t = pilas.actores.Texto("Has perdido ...")
+        t = pilas.actores.Texto("uuyyy !!!")
         t.escala = 0
-        t.escala = [1], 0.5
+        t.escala = [1], 0.2
+        t.rotacion = 60
+        t.rotacion = [0], 0.2
+
+        def eliminar_texto():
+            t.eliminar()
+
+        pilas.mundo.agregar_tarea(1.5, eliminar_texto)
+
+    def regresar_al_vuelo(self):
+        self.estado = Volando(self)
+
+    def sonreir(self):
+        self.contador_sonreir = 40
 
 class Enemigo(pilas.actores.Bomba):
 
@@ -117,11 +200,10 @@ class Enemigo(pilas.actores.Bomba):
         self.x -= 5
         pilas.actores.Bomba.actualizar(self)
 
-class Item(pilas.actores.Actor):
+class Netbook(pilas.actores.Actor):
 
     def __init__(self):
-        pilas.actores.Actor.__init__(self, 'estrella.png')
-        self.escala = 0.5
+        pilas.actores.Actor.__init__(self, 'netbook.png')
         self.izquierda = 320
         self.y = random.randint(-210, 210)
 
@@ -129,6 +211,24 @@ class Item(pilas.actores.Actor):
         self.izquierda -= 5
 
         if self.derecha < -320:
+            self.eliminar()
+
+    def capturar(self):
+        self.eliminar()
+        EfectoNetbookLiberada(self.x, self.y)
+
+
+class EfectoNetbookLiberada(pilas.actores.Actor):
+
+    def __init__(self, x, y):
+        pilas.actores.Actor.__init__(self, 'netbook_liberada.png', x=x, y=y)
+
+    def actualizar(self):
+        self.izquierda -= 8
+        self.escala += 0.1
+        self.transparencia += 3
+
+        if self.transparencia > 90:
             self.eliminar()
 
 
@@ -163,32 +263,34 @@ class Nube(pilas.actores.Actor):
 
 pilas.iniciar()
 
-fondo = pilas.fondos.Fondo('data/nubes.png')
-puntos = pilas.actores.Puntaje(x=-290, y=210)
+fondo = pilas.fondos.Fondo('data/fondo.png')
+puntos = pilas.actores.Puntaje(y=210)
+jugador = Jugador()
+puntos.izquierda = jugador.derecha + 10
+
 vaca = Vaca()
 items = []
 enemigos = []
 
-def crear_item():
-    un_item = Item()
+def crear_netbook():
+    un_item = Netbook()
     items.append(un_item)
     return True
 
-pilas.mundo.agregar_tarea(2, crear_item)
+pilas.mundo.agregar_tarea(2, crear_netbook)
 
 
-
-
-def cuanto_toca_item(v, i):
-    i.eliminar()
+def cuanto_toca_item(vaca, netbook):
+    netbook.capturar()
+    vaca.sonreir()
     puntos.aumentar(10)
     puntos.escala = 2
     puntos.escala = [1], 0.2
     puntos.rotacion = random.randint(30, 60)
     puntos.rotacion = [0], 0.2
+    jugador.mostrar_cara_gana()
 
 pilas.mundo.colisiones.agregar(vaca, items, cuanto_toca_item)
-
 
 def crear_enemigo():
     un_enemigo = Enemigo()
@@ -201,6 +303,7 @@ pilas.mundo.agregar_tarea(3.3, crear_enemigo)
 def cuanto_toca_enemigo(vaca, enemigo):
     vaca.perder()
     enemigo.eliminar()
+    jugador.mostrar_cara_pierde()
 
 
 pilas.mundo.colisiones.agregar(vaca, enemigos, cuanto_toca_enemigo)
